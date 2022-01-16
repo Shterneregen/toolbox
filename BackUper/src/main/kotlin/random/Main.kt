b465244651e70482ed3fef6f9acf25b01cb256ab
shterneregen <shterneregen@gmail.com>
2022-01-17 00:35:22 +0300

    BackUper improvements

--- BackUper/src/main/kotlin/random/Main.kt
+++ BackUper/src/main/kotlin/random/Main.kt
@@ -16,46 +16,48 @@ fun main(args: Array<String>) {
             val propFile = args[1 + args.indexOf("-p")]
             createBackUps(propFile, createInNewFolder)
         }
-        else -> coloredPrintln("Specify property file using -p flag", Colors.RED.code)
+        else -> coloredPrintln("Specify property file using -p flag", Color.RED)
     }
 }
 
 fun createBackUps(propFile: String, createInNewFolder: Boolean) {
     if (!File(propFile).exists()) {
-        coloredPrintln("$propFile doesn't exist!", Colors.RED.code)
+        coloredPrintln("$propFile doesn't exist!", Color.RED)
         return
     }
     val prop = Properties()
     prop.load(FileInputStream(propFile))
 
-    val folderForBkps = File(prop["folderForBkps"] as String).also { folder -> if (!folder.exists()) folder.mkdir() }
-    val files = prop.getProperty("files").split(",")
-    files.forEach { filePath -> createBkp(filePath, folderForBkps, createInNewFolder) }
+    val folderForBkps = File(prop["folderForBkps"] as String)
+    println("Backup folder: ${folderForBkps.path}")
+    prop.getProperty("files")?.split(",")
+        ?.onEach { filePath -> createBkp(filePath, folderForBkps, createInNewFolder) }
+        ?: run { coloredPrintln("No files to back up found", Color.RED) }
 }
 
 fun createBkp(filePath: String, folderForBkps: File, createInNewFolder: Boolean) {
     val file = File(filePath)
     if (!file.exists()) {
-        coloredPrintln("$filePath doesn't exist!", Colors.RED.code)
+        coloredPrintln("$filePath doesn't exist!", Color.RED)
         return
     }
 
-    val target = if (createInNewFolder) File("${folderForBkps.name}/${getCurrentDay()}/${file.name}")
-    else File("${folderForBkps.name}/${file.name}")
+    val target = if (createInNewFolder) File("${folderForBkps.path}/${getCurrentDay()}/${file.name}")
+    else File("${folderForBkps.path}/${file.name}")
 
     when {
         target.exists() && getFileSize(file) == getFileSize(target) -> {
-            coloredPrintln("$filePath has not been modified!", Colors.GREY.code)
+            coloredPrintln("${file.path} has not been modified!", Color.GREY)
         }
         else -> {
             file.copyTo(target, true)
-            coloredPrintln("$filePath copied!", Colors.GREEN.code)
+            println("${file.path} copied!\t-->\t${coloredText(target.path, Color.GREEN)}")
         }
     }
 }
 
-fun coloredPrintln(text: String, colorCode: String = Colors.GREY.code) = println(coloredText(text, colorCode))
-fun coloredText(text: String, colorCode: String = Colors.GREY.code) = "$colorCode$text${Colors.RESET.code}"
+fun coloredPrintln(text: String, color: Color = Color.GREY) = println(coloredText(text, color))
+fun coloredText(text: String, color: Color = Color.GREY) = "${color.code}$text${Color.RESET.code}"
 
 fun fileAttributes(file: File): BasicFileAttributes =
     Files.readAttributes(file.toPath(), BasicFileAttributes::class.java)
@@ -65,6 +67,6 @@ fun getFileSize(file: File) = fileAttributes(file).size()
 fun getCurrentDay(): String = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy"))
 
 
-enum class Colors(val code: String) {
+enum class Color(val code: String) {
     RED("\u001b[31m"), GREEN("\u001b[32m"), GREY("\u001b[37m"), RESET("\u001b[0m")
 }
